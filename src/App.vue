d74c127ab2209f3ed31809f6969132f59d93155b
Felipe Cancino <f.cancino@novatize.com>
2022-09-12 10:48:47 -0400

    add view home

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/manager">Career Manager</router-link> |
     <router-link to="/player">Career Player</router-link> |
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
 
@@ -16,17 +16,8 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
+body {
+  margin: 0;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolorem perspiciatis, veritatis nulla deleniti tenetur sequi consequuntur provident blanditiis. Repellendus accusamus temporibus dignissimos dolorem quos cum reprehenderit eveniet vitae dicta?</p>
+    <nav class="principal-navigation">
+      <router-link to="/manager"><span>Career Manager</span></router-link>
+      <router-link to="/player"><span>Career Player</span></router-link>
+    </nav>
   </div>
 </template>
 
@@ -15,3 +18,73 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .principal-navigation {
+    height: 100vh;
+    display: block;
+    position: relative;
+    a {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #cacaca;
+      font-size: 3rem;
+      color: #fff;
+      text-decoration: none;
+      transition: .2s linear;
+      overflow: hidden;
+      position: absolute;
+      top: 0;
+      left: 0;
+      span {
+        position: relative;
+        &:before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border-top: 5px solid #fff;
+          border-bottom: 5px solid #fff;
+          transform: scale(1.5);
+          transition: .1s linear;
+          opacity: 0;
+        }
+      }
+      &:first-child {
+        background-image: url(../assets/career-mode-manager-background.jpeg);
+        background-size: cover;
+        background-position: center;
+        clip-path: polygon(0 0%, 75% 0%, 0% 154%, 0% 100%);
+        justify-content: flex-start;
+        z-index: 1;
+        span {
+          margin-left: 1.5rem;
+        }
+      }
+      &:last-child {
+        background-image: url(../assets/career-mode-background-2.jpeg);
+        background-size: cover;
+        background-position: -27% 0;
+        z-index: 0;
+        background-repeat: no-repeat;
+        justify-content: flex-end;
+        span {
+          margin-right: 1.5rem;
+        }
+      }
+      &:hover {
+        span {
+          &:before {
+            transform: scale(1);
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+</style>
